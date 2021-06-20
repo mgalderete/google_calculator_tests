a684a5ae3ff86b264cb5a959efc8f2b837cd7e50
mgalderete <marcosalderete90@gmail.com>
2021-06-20 17:27:15 -0500

    Returning removed locators due these are needed for TC_CALCULATOR_001

--- GoogleCalculatorTests/Pages/CalculatorPage.cs
+++ GoogleCalculatorTests/Pages/CalculatorPage.cs
@@ -33,6 +33,87 @@ namespace GoogleCalculatorTests.Pages
         {
             get { return _driver.FindElement(By.XPath("//div[.='AC']")); }
         }
+
+        public IWebElement SevenButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='7']")); }
+        }
+
+        public IWebElement EightButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='8']")); }
+        }
+
+        public IWebElement NineButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='9']")); }
+        }
+
+        public IWebElement DivisionButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='÷']")); }
+        }
+
+        public IWebElement FourButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='4']")); }
+        }
+
+        public IWebElement FiveButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='5']")); }
+        }
+
+        public IWebElement SixButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='6']")); }
+        }
+
+        public IWebElement MultiplicationButton
+        {
+            get { return _driver.FindElement(By.CssSelector("[jsname='YovRWb']")); }
+        }
+
+        public IWebElement OneButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='1']")); }
+        }
+
+        public IWebElement TwoButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='2']")); }
+        }
+
+        public IWebElement ThreeButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='3']")); }
+        }
+
+        public IWebElement SubtractionButton
+        {
+            get { return _driver.FindElement(By.CssSelector("[jsname='pPHzQc']")); }
+        }
+
+        public IWebElement ZeroButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='0']")); }
+        }
+
+        public IWebElement DecimalButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='.']")); }
+        }
+
+        public IWebElement EqualslButton
+        {
+            get { return _driver.FindElement(By.ClassName("UUhRt")); }
+        }
+
+        public IWebElement AddtionButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='+']")); }
+        }
+
         #endregion
 
         /// <summary>
@@ -62,10 +143,10 @@ namespace GoogleCalculatorTests.Pages
                     switch (digit)
                     {
                         case 'x':
-                            CalculatorContainer.FindElement(By.CssSelector("[jsname='YovRWb']")).Click();
+                            MultiplicationButton.Click();
                             break;
                         case '-':
-                            CalculatorContainer.FindElement(By.CssSelector("[jsname='pPHzQc']")).Click();
+                            SubtractionButton.Click();
                             break;
                         default:
                             CalculatorContainer.FindElement(By.XPath($"//div[.='{digit}']")).Click();
