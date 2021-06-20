4d3b79561aed89834abdc9a26702c9c745cb6cf2
mgalderete <marcosalderete90@gmail.com>
2021-06-20 03:30:53 -0500

    Adding calculator page object locators

--- GoogleCalculatorTests/Pages/CalculatorPage.cs
+++ GoogleCalculatorTests/Pages/CalculatorPage.cs
@@ -16,6 +16,103 @@ namespace GoogleCalculatorTests.Pages
             _driver = driver;
         }
 
+        #region Locators
+        public IWebElement CalculatorContainer
+        {
+            get { return _driver.FindElement(By.ClassName("tyYmIf")); }
+        }
+
+        public IWebElement CalculatorScreen
+        {
+            get { return _driver.FindElement(By.ClassName("XH1CIc")); }
+        }
+
+        public IWebElement ACButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='AC']")); }
+        }
+
+        public IWebElement SevenButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='7']")); }
+        }
+
+        public IWebElement EightButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='8']")); }
+        }
+
+        public IWebElement NineButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='9']")); }
+        }
+
+        public IWebElement DivisionButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='÷']")); }
+        }
+
+        public IWebElement FourButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='4']")); }
+        }
+
+        public IWebElement FiveButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='5']")); }
+        }
+
+        public IWebElement SixButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='6']")); }
+        }
+
+        public IWebElement MultiplicationButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='x']")); }
+        }
+
+        public IWebElement OneButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='1']")); }
+        }
+
+        public IWebElement TwoButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='2']")); }
+        }
+
+        public IWebElement ThreeButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='3']")); }
+        }
+
+        public IWebElement SubstractionButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='-']")); }
+        }
+
+        public IWebElement ZeroButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='0']")); }
+        }
+
+        public IWebElement DecimalButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='.']")); }
+        }
+
+        public IWebElement EqualslButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='=']")); }
+        }
+
+        public IWebElement AddtionButton
+        {
+            get { return _driver.FindElement(By.XPath("//div[.='+']")); }
+        }
+
+        #endregion
 
     }
 }
